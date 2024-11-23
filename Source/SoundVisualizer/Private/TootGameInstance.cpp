9b4d5c29a96c15e90dded053812f33a44ca0a2d7
TOOTzoe <toot@tootzoe.com>
2024-11-23 23:14:21 +0700

    Initial commit

---
+++ Source/SoundVisualizer/Private/TootGameInstance.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TootGameInstance.h"
+
+UTootGameInstance::UTootGameInstance()
+{
+
+
+
+	UE_LOG(LogTemp, Warning, TEXT("This UTootGameInstance msg.........."));
+
+
+}
---
+++ Source/SoundVisualizer/Public/TootGameInstance.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "TootGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOUNDVISUALIZER_API UTootGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+
+public:
+	UTootGameInstance();
+	
+	
+	
+};
