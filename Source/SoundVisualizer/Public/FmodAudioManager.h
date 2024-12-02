97e25c5d33fce9313af00583de7dbf153c874961
TOOTzoe <toot@tootzoe.com>
2024-12-03 00:28:43 +0700

    First step specturm works now....

---
+++ Source/SoundVisualizer/Public/FmodAudioManager.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "FmodAudioManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOUNDVISUALIZER_API UFmodAudioManager : public UObject
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
